cc021c6dff8a80a205297d7d674408135215bfdf
CarterGillis1997 <92799737+CarterGillis1997@users.noreply.github.com>
2023-02-23 10:37:33 -0500

    Transition Message, CPU Target Selection
    Turn Transition Message is implemented and on hold for now. Enemy can select a random space on the player board to fire at. If it is a hit the enemy will shoot near that spot again based on what hit markers are placed near the spot it shot at.

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ var enemyBoard = {
             length:5
         }
     },
-    quickCoords:[],
+    quickCoords:[], //Indexed
     matrix:[
         [0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0],
@@ -81,7 +81,14 @@ var enemyBoard = {
         [0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0]
-    ]
+    ],
+    prediction:{
+        "small_ship":false,
+        "gun_boat":false,
+        "clipper":false,
+        "battleship":false,
+        "carrier":false
+    }
 }
 function startGame(){
 
@@ -300,7 +307,8 @@ function enemySetup(){
                         if(enemyBoard.matrix[anchorY][g] == 0){
                            let part = Object.keys(enemyBoard.ships[shipNames[i]].parts)[partCounter]
                             //Have the part name. Have to set the part coords here
-                           enemyBoard.matrix[anchorY][g] = 1
+                            enemyBoard.ships[shipNames[i]].parts[part] = [g, anchorY]
+                            enemyBoard.matrix[anchorY][g] = 1
                            partCounter += 1
                         }else{
                             i-=1
@@ -312,7 +320,8 @@ function enemySetup(){
                         console.log(enemyBoard.matrix[g][anchorX])
                         if(enemyBoard.matrix[g][anchorX] == 0){
                            let part = Object.keys(enemyBoard.ships[shipNames[i]].parts)[partCounter]
-                           enemyBoard.matrix[g][anchorX] = 1
+                            enemyBoard.ships[shipNames[i]].parts[part] = [anchorX, g]
+                            enemyBoard.matrix[g][anchorX] = 1
                             partCounter += 1
                         }else{
                             i-=1
@@ -324,7 +333,8 @@ function enemySetup(){
                         console.log(enemyBoard.matrix[g][anchorX])
                         if(enemyBoard.matrix[g][anchorX] == 0){
                            let part = Object.keys(enemyBoard.ships[shipNames[i]].parts)[partCounter]
-                           enemyBoard.matrix[g][anchorX] = 1
+                            enemyBoard.ships[shipNames[i]].parts[part] = [anchorX, g]
+                            enemyBoard.matrix[g][anchorX] = 1
                             partCounter += 1
                         }else{
                             i-=1
@@ -336,7 +346,8 @@ function enemySetup(){
                         console.log(enemyBoard.matrix[anchorY][g])
                         if(enemyBoard.matrix[anchorY][g] == 0){
                            let part = Object.keys(enemyBoard.ships[shipNames[i]].parts)[partCounter]
-                           enemyBoard.matrix[anchorY][g] = 1
+                            enemyBoard.ships[shipNames[i]].parts[part] = [g, anchorY]
+                            enemyBoard.matrix[anchorY][g] = 1
                             partCounter += 1
                         }else{
                             i-=1
@@ -352,9 +363,18 @@ function enemySetup(){
         turnTransition("Game Start - Your Turn", 'player')
     }
 
-function turnTransition(message, nextTurn){
+function turnTransition(message = 'I am a placeholder', nextTurn){
 
     //Displaying Message
+    $(".messageBox").addClass("showMessage").on("animationend",()=>{
+        $(".messageBox").off("animationend").removeClass("showMessage").css({"left":"calc(50% - 35%)"})
+        setTimeout(()=>{
+            $(".messageBox").addClass("hideMessage").on("animationend",()=>{
+                $(".messageBox").off("animationend").css("left","100%").removeClass("hideMessage")
+            })
+        }, 500)
+    })
+    $(".messageBox h1").text(message)
 
     if(nextTurn == 'player'){
         playerTurn();
@@ -366,12 +386,145 @@ function turnTransition(message, nextTurn){
 function playerTurn(){
     $(".shotBoard .cell").on("click",(event)=>{
         let coords = getCoords(event.currentTarget)
+        if($(event.currentTarget).children("img").length == 1){
         if(enemyBoard.matrix[coords[0] - 1][coords[1] - 1] == 1){
             $(event.currentTarget).empty().addClass("hit").append($("<div/>").addClass("hitMarker").css({"transform":`rotate(${Math.floor(Math.random()*360)}deg) translate(${Math.floor(Math.random()*10)-5}px,${Math.floor(Math.random()*10)-5}px)`})).css({"display":"flex","justify-content":"center","align-items":"center"})
         }else{
             $(event.currentTarget).empty().append($("<div/>").addClass("marker").css({"transform":`rotate(${Math.floor(Math.random()*360)}deg) translate(${Math.floor(Math.random()*10)-5}px,${Math.floor(Math.random()*10)-5}px)`})).css({"display":"flex","justify-content":"center","align-items":"center"});
+            $(".shotBoard .cell").off("click")
+            turnTransition("Miss - CPU Turn", 'enemy')
         }
+    }else{
+
+    }
     })
+}
+
+function cpuTurn(target = false){
+    $("body").addClass("wait")
+    //Does it think there are revealed ships that have not sunk
+
+    if($(".lastShot").length !== 0){
+
+    }
+
+    //Random Shot
+    let x = Math.floor(Math.random()*9) + 1 // 1 - 10
+    let y = Math.floor(Math.random()*9) + 1 // 1 - 10
+    target = $(`.yourBoard #row${y}`).children(`#col${x}`);
+    if($(target).children("img").length == 0){
+        setTimeout(()=>{
+        $(target).append($("<div/>").addClass("marker"))
+        $("body").removeClass("wait")
+        turnTransition("Miss - Your Turn", 'player')
+    }, Math.floor(Math.random()*2000) + 2000)
+    }else{
+        setTimeout(()=>{
+            $(".lastHit").removeClass("lastHit")
+            $(target).append($("<div/>").addClass("hitMarker").addClass("lastHit"))
+            //Find if the edges are discovered
+            let match = [0,0,0,0] //Left, Top, Right, Down
+            //left
+            for(let i = x; i >= 0; i--){
+                console.log("left", x, i)
+                console.log($(`.yourBoard #row${y}`).children(`#col${i}`))
+                if($(`.yourBoard #row${y}`).children(`#col${i}`).children('.hitMarker').length !== 0){
+                    match[0] +=1
+                }else{
+                    target.push([i,y])
+                    break;
+                }
+            }
+            //Top
+            for(let i = y; i >= 0; i--){
+                console.log("top", y, i)
+                console.log($(`.yourBoard #row${i}`).children(`#col${x}`))
+                if($(`.yourBoard #row${i}`).children(`#col${x}`).children('.hitMarker').length !== 0){
+                    match[1] +=1
+                }else{
+                    target.push([x,i])
+                    break;
+                }
+            }
+            //Right
+            for(let i = x; i <= 10; i++){
+                console.log("right", x, i)
+                console.log($(`.yourBoard #row${y}`).children(`#col${i}`))
+                if($(`.yourBoard #row${y}`).children(`#col${i}`).children('.hitMarker').length !== 0){
+                    match[2] +=1
+                }else{
+                    target.push([i,y])
+                    break;
+                }
+            }
+            //Down
+            for(let i = y; i <= 10; i++){
+                console.log("down", y, i)
+                console.log($(`.yourBoard #row${i}`).children(`#col${x}`))
+                if($(`.yourBoard #row${i}`).children(`#col${x}`).children('.hitMarker').length !== 0){
+                    match[3] +=1
+                }else{
+                    target.push([x,i])
+                    break;
+                }
+            }
+            let aim;
+            //Decide where to shoot next
+            if(match[0] + match[2] >= match[1] + match[3]){
+                //left or right
+                let rng = Math.floor(Math.random()*2)
+                for(let i = 0; i < 2; i++){
+                if(rng == 0){
+                    //Left
+                    let x1 = x - match[0]
+                    console.log("Left")
+                    if($(`.yourBoard #row${y}`).children(`#col${x1}`).children(".marker").length == 0){
+                        aim = [x1, y]
+                        break;
+                    }else{
+                        rng = 1
+                        i = 0;
+                    }
+                }else{
+                    let x1 = x + match[0]
+                    console.log("right")
+                    if($(`.yourBoard #row${y}`).children(`#col${x1}`).children(".marker").length == 0){
+                        aim = [x1, y]
+                        break;
+                    }else{
+                        rng = 0;
+                        i = 0;
+                    }
+                }
+            }
+            }else{
+                //Up or Down
+                let rng = Math.floor(Math.random()*2)
+                for(let i = 0; i < 2; i++){
+                if(rng == 0){
+                    //Up
+                    let y1 = y - match[1]
+                    console.log("Up")
+                    if($(`.yourBoard #row${y1}`).children(`#col${x}`).children(".marker").length == 0){
+                        aim = [x1, y]
+                        break;
+                    }else{
+                        rng = 0;
+                        i = 0;
+                    }
+                }else{
+                    let y1 = y + match[3];
+                    console.log("Down")
+                    console.log($(`.yourBoard #row${y1}`).children(`#col${x}`))
+                }
+            }
+        }
+            console.log(aim)
+            //Currently gets the correct cell to fire at next
+            //Need to tell it to do that now
+            cpuTurn(target);
+        }, Math.floor(Math.random()*2000) + 2000)
+    }
 }
 
 function getCoords(element){
@@ -383,6 +536,10 @@ function getCoords(element){
 $("body").on("keydown",(event)=>{
     if(event.key == 'p'){
         enemySetup()
+    }else if(event.key == 'o'){
+        turnTransition();
+    }else if(event.key == 'i'){
+            $(".yourBoard .cell:nth-child(even)").append($("<img>"))
     }
 })
 
